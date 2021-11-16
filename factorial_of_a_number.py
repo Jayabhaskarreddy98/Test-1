efcdf35f316bb44cf77a20357ee7a6520de2d1bc
Jayabhaskarreddy Mummasani <93757919+Jayabhaskarreddy98@users.noreply.github.com>
2021-11-16 14:29:49 +0530

    Add files via upload

---
+++ factorial_of_a_number.py
@@ -0,0 +1,10 @@
+num = int(input('Enter the Number -> '))
+
+fact = 1
+
+if num < 0:
+    print('Factorial of negative number is not defined')
+else:
+    for i in range(1, num + 1):
+        fact *= i
+    print('Factorial of {} is {}'.format(num, fact))
